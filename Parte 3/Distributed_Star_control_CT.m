ee4c84c3bf6341394ceba59c60553c7e65f33df9
MichelePederzoli <michele.pederzoli.st@gmail.com>
2025-02-06 17:06:34 +0100

    [LMI]: division in CT and DT of all the structures

---
+++ Parte 3/Distributed_Star_control_CT.m
@@ -0,0 +1,202 @@
+close all 
+clear all
+clc
+
+addpath('Funzioni/CT');
+addpath('Funzioni/DT');
+addpath('Funzioni');
+
+%% MATRIXES DEFINITION
+
+%Array of masses
+% Marcello Farina, 22/10/2020
+
+N=9;
+m=[3 2 3 2 3 2 4 1 2];
+k=[0 0.1 0 0.2 0 0 0 0 0
+   0.1 0 0.2 0 0.1 0 0 0 0
+   0 0.2 0 0 0 0.3 0 0 0
+   0.2 0 0 0 0.2 0 0.5 0 0
+   0 0.1 0 0.2 0 0.7 0 0.1 0
+   0 0 0.3 0 0.7 0 0 0 0.4
+   0 0 0 0.5 0 0 0 0.9 0
+   0 0 0 0 0.1 0 0.9 0 0.1
+   0 0 0 0 0 0.4 0 0.1 0];
+
+h=[0 0.13 0 0.13 0 0 0 0 0
+   0.13 0 0.2 0 0.14 0 0 0 0
+   0 0.2 0 0 0 0.3 0 0 0
+   0.13 0 0 0 0.2 0 0.1 0 0
+   0 0.14 0 0.2 0 0.2 0 0.1 0
+   0 0 0.3 0 0.2 0 0 0 0.4
+   0 0 0 0.1 0 0 0 0.1 0
+   0 0 0 0 0.1 0 0.1 0 0.4
+   0 0 0 0 0 0.4 0 0.4 0];
+
+A=zeros(N*4,N*4);
+n = N*2;
+B=[];
+for i=1:N
+    for j=1:N
+        if i==j
+            Ai=[0 1;-sum(k(i,1:N))/m(i) -sum(h(i,1:N))/m(i)];
+        else
+            Ai=[0 0;k(i,j)/m(i) h(i,j)/m(i)];
+        end
+        Ac{i,j}=blkdiag(Ai,Ai);
+        A((i-1)*4+1:4*i,(j-1)*4+1:4*j)=Ac{i,j};
+    end
+    B=blkdiag(B,blkdiag([0 1/m(i)]',[0 1/m(i)]'));
+end
+C=eye(4*N);
+
+%% Discretization
+% Time costant analysis
+Ts= 0.1; %Shannon Theorem on signal sampling (fs=2*fc at least) 
+[F,G,H,W,Ts]=ssdata(c2d(ss(A,B,C,[]),Ts));
+
+%% Decomposition
+
+for i=1:2:(N*2-1)
+    Bd{ceil(i/2)}=B(:,i:i+1);
+    Gd{ceil(i/2)}=G(:,i:i+1);
+end
+
+for i=1:4:N*4
+    Cd{ceil(i/4)}=C(i:i+3,:);
+    Hd{ceil(i/4)}=H(i:i+3,:);
+end
+
+%% Stability
+% Continuous time
+Autovalori_CT = eig(A);
+isStable = true;
+for i = 1:length(Autovalori_CT)
+    if real(Autovalori_CT(i)) > 0
+        isStable = false;
+    end
+end
+if isStable
+    disp('System is Stable in CT');
+else
+    disp('System is Unstable in CT');
+end
+
+real_parts_CT = real(Autovalori_CT);
+
+spectral_abscissa = max(real_parts_CT);   % Spectral Abscissa
+disp(['Spectral Abscissa: ', num2str(spectral_abscissa)]);
+
+%% Control Structures
+alpha = 2;  % Must be positive, the negative sign is already considered in the LMI computation
+rho_DT = exp(alpha*Ts);
+center = 20; % Must be positive, the negative sign is already considered in the LMI computation
+radius = 1;
+
+% Distributed LMI Performance
+ContStruc_Distr_star=eye(N);
+for i=2:N
+    ContStruc_Distr_star(1,i)=1;
+    ContStruc_Distr_star(i,1)=1;
+end
+[star_fm]=di_fixed_modes(A,Bd,Cd,N,ContStruc_Distr_star,3);
+
+% Continuous Time 
+[K_star_CT,rho_star_CT,feas_star_CT]=LMI_CT_Stability(A,Bd,Cd,N,ContStruc_Distr_star); % LMI for stability
+[K_star_CT_perf,rho_star_CT_perf,feas_star_CT_perf]=LMI_CT_Performance(A,Bd,Cd,N,ContStruc_Distr_star,alpha); % LMI for performance
+[K_star_CT_sector,rho_star_CT_sector,feas_star_CT_sector]=LMI_CT_Sector(A,Bd,Cd,N,ContStruc_Centr,angle) % LMI for sector delimited area
+[K_star_CT_effort,rho_star_CT_effort,feas_star_CT_effort]=LMI_CT_Effort(A,Bd,Cd,N,ContStruc_Centr,alpha_L,alpha_Y) % LMI for sector delimited area
+[K_star_CT_H2,rho_star_CT_H2,feas_star_CT_H2]=LMI_CT_H2(A,Bd,Cd,N,ContStruc_Centr) % LMI for H2
+
+%% Display
+
+ disp('Results (Continuous-time):')
+ disp(['-  Distributed_Star_CT: Feasibility=',num2str(feas_star_CT),', rho=',num2str(rho_star_CT),', FM=',num2str(star_fm),'.'])
+ disp(['-  Distributed_Star_CT_Perf: Feasibility=',num2str(feas_star_CT_perf),', rho=',num2str(rho_star_CT_perf),', FM=',num2str(star_fm),'.'])
+ disp(['-  Distributed_Star_CT_Sector: Feasibility=',num2str(feas_star_CT_sector),', rho=',num2str(rho_star_CT_sector),', FM=',num2str(star_fm),'.'])
+ disp(['-  Distributed_Star_CT_Effort: Feasibility=',num2str(feas_star_CT_effort),', rho=',num2str(rho_star_CT_effort),', FM=',num2str(star_fm),'.'])
+ disp(['-  Distributed_Star_CT_H2: Feasibility=',num2str(feas_star_CT_H2),', rho=',num2str(rho_star_CT_H2),', FM=',num2str(star_fm),'.'])
+
+%% Plots
+Gtot=[];
+Htot=[];
+Btot=[];
+Ctot=[];
+for i=1:N
+    Btot=[B,Bd{i}];
+    Ctot=[C
+        Cd{i}];
+    Gtot=[G,Gd{i}];
+    Htot=[H
+        Hd{i}];
+end
+
+% simulation data
+Tfinal=10;
+T=0:0.01:Tfinal;
+% Random initial condition 
+min_x0 = 0;
+max_x0 = 1;
+random_number = min_x0 + (max_x0-min_x0) .* rand(1,1);  % Generate a random number between [min_x0, max_x0]
+x0 = repmat(random_number,36,1);
+
+k = 0;
+
+
+% CT Simulation 
+for t=T
+    k=k+1;
+
+    % state computation
+    x_star_free(:,k)=expm(A*t)*x0; % No control
+    x_star_CT(:,k)=expm((A+B*K_c_CT)*t)*x0;
+    x_star_CT_perf(:,k)=expm((A+B*K_c_CT_perf)*t)*x0;
+    x_star_CT_sector(:,k)=expm((A+B*K_star_CT_sector)*t)*x0;
+    x_star_CT_effort(:,k)=expm((A+B*K_star_CT_effort)*t)*x0;
+    x_star_CT_H2(:,k)=expm((A+B*K_star_CT_H2)*t)*x0;
+
+    % control variable
+    u_star_CT(:,k) = K_c_CT * x_star_CT(:,k);
+    u_star_CT_perf(:,k) = K_c_CT_perf * x_star_CT_perf(:,k);
+    u_star_CT_sector(:,k) = K_star_CT_sector * x_star_CT_sector(:,k);
+    u_star_CT_effort(:,k) = K_star_CT_effort * x_star_CT_effort(:,k);
+    u_star_CT_H2(:,k) = K_star_CT_H2 * x_star_CT_H2(:,k);
+end
+
+% Continuous Time figure
+% Primo grafico: posizione lungo X
+figure
+plot(T, x_c_free(1,:),T, x_star_CT(1,:), T, x_star_CT_perf(1,:), T, x_star_CT_sector(1,:),T,x_star_CT_effort(1,:),T,x_star_CT_H2(1,:)) % Position of the first Mass along x direction
+title('CT controller Position in X')  
+grid on
+legend('No control','CT Stability', 'CT Performance', 'CT Sector', 'CT Effort', 'CT H2') % Aggiunge la legenda
+xlabel('Time (s)') % Etichetta dell'asse x
+ylabel('Position (X)') % Etichetta dell'asse y
+
+% Secondo grafico: posizione lungo Y
+figure
+plot(T, x_c_free(3,:),T, x_star_CT(3,:), T, x_star_CT_perf(3,:), T, x_star_CT_sector(3,:),T, x_star_CT_effort(3,:),T, x_star_CT_H2(3,:)) % Position of the first Mass along y direction
+title('CT controllers Position in Y')
+grid on
+legend('No control','CT Stability', 'CT Performance', 'CT Sector','CT Effort', 'CT H2') % Aggiunge la legenda
+xlabel('Time (s)') % Etichetta dell'asse x
+ylabel('Position (Y)') % Etichetta dell'asse y
+
+% Terzo grafico: variabile di controllo U lungo X
+figure
+plot(T, u_star_CT(1,:),T, u_star_CT_perf(1,:),T, u_star_CT_sector(1,:),T, u_star_CT_effort(1,:),T, u_star_CT_H2(1,:)) % variabile di controllo lungo x
+title('CT control variable in X')
+grid on
+legend('CT Stability', 'CT Performance', 'CT Sector','CT Effort', 'CT H2') % Aggiunge la legenda
+xlabel('Time (s)') % Etichetta dell'asse x
+ylabel('Control action (U(x))') % Etichetta dell'asse x
+
+figure
+plot(T, u_star_CT(2,:),T, u_star_CT_perf(2,:),T, u_star_CT_sector(2,:),T, u_star_CT_effort(2,:),T, u_star_CT_H2(2,:)) % variabile di controllo lungo y
+title('CT control variable in Y')
+grid on
+legend('CT Stability', 'CT Performance', 'CT Sector','CT Effort', 'CT H2') % Aggiunge la legenda
+xlabel('Time (s)') % Etichetta dell'asse y
+ylabel('Control action (U(y))') % Etichetta dell'asse y
+
+%Eigen = eig(A-B*K_c_CT_effort)
---
+++ Parte 3/Distributed_String_control_DT.m
@@ -0,0 +1,193 @@
+close all 
+clear all
+clc
+
+addpath('Funzioni/CT');
+addpath('Funzioni/DT');
+addpath('Funzioni');
+
+%% MATRIXES DEFINITION
+
+%Array of masses
+% Marcello Farina, 22/10/2020
+
+N=9;
+m=[3 2 3 2 3 2 4 1 2];
+k=[0 0.1 0 0.2 0 0 0 0 0
+   0.1 0 0.2 0 0.1 0 0 0 0
+   0 0.2 0 0 0 0.3 0 0 0
+   0.2 0 0 0 0.2 0 0.5 0 0
+   0 0.1 0 0.2 0 0.7 0 0.1 0
+   0 0 0.3 0 0.7 0 0 0 0.4
+   0 0 0 0.5 0 0 0 0.9 0
+   0 0 0 0 0.1 0 0.9 0 0.1
+   0 0 0 0 0 0.4 0 0.1 0];
+
+h=[0 0.13 0 0.13 0 0 0 0 0
+   0.13 0 0.2 0 0.14 0 0 0 0
+   0 0.2 0 0 0 0.3 0 0 0
+   0.13 0 0 0 0.2 0 0.1 0 0
+   0 0.14 0 0.2 0 0.2 0 0.1 0
+   0 0 0.3 0 0.2 0 0 0 0.4
+   0 0 0 0.1 0 0 0 0.1 0
+   0 0 0 0 0.1 0 0.1 0 0.4
+   0 0 0 0 0 0.4 0 0.4 0];
+
+A=zeros(N*4,N*4);
+n = N*2;
+B=[];
+for i=1:N
+    for j=1:N
+        if i==j
+            Ai=[0 1;-sum(k(i,1:N))/m(i) -sum(h(i,1:N))/m(i)];
+        else
+            Ai=[0 0;k(i,j)/m(i) h(i,j)/m(i)];
+        end
+        Ac{i,j}=blkdiag(Ai,Ai);
+        A((i-1)*4+1:4*i,(j-1)*4+1:4*j)=Ac{i,j};
+    end
+    B=blkdiag(B,blkdiag([0 1/m(i)]',[0 1/m(i)]'));
+end
+C=eye(4*N);
+
+%% Discretization
+% Time costant analysis
+Ts= 0.1; %Shannon Theorem on signal sampling (fs=2*fc at least) 
+[F,G,H,W,Ts]=ssdata(c2d(ss(A,B,C,[]),Ts));
+
+%% Decomposition
+
+for i=1:2:(N*2-1)
+    Bd{ceil(i/2)}=B(:,i:i+1);
+    Gd{ceil(i/2)}=G(:,i:i+1);
+end
+
+for i=1:4:N*4
+    Cd{ceil(i/4)}=C(i:i+3,:);
+    Hd{ceil(i/4)}=H(i:i+3,:);
+end
+
+%% Stability
+% Discrete time 
+Autovalori_DT = eig(F);
+isStable_2 = true;
+for i = 1:length(Autovalori_DT)
+    if abs(real(Autovalori_DT(i))) > 1+1e-4
+        isStable_2 = false;
+    end
+end
+if isStable_2
+    disp('System is Stable in DT');
+else
+    disp('System is Unstable in DT');
+end
+
+moduli = abs(Autovalori_DT);
+spectral_radius = max(moduli);  % Spectral Radius
+disp(['Spectral Radius: ', num2str(spectral_radius)]);
+
+%% Control Structures
+alpha = 2;  % Must be positive, the negative sign is already considered in the LMI computation
+rho_DT = exp(alpha*Ts);
+center = 20; % Must be positive, the negative sign is already considered in the LMI computation
+radius = 1;
+
+% Distributed LMI Performance
+ContStruc_Distr_string=eye(N);
+for i=1:N-1
+    ContStruc_Distr_string(i,i+1)=1;
+    ContStruc_Distr_string(i+1,i)=1;
+end
+[string_fm_DT]=di_fixed_modes(F,Gd,Hd,N,ContStruc_Distr_string,3);
+
+% Discrete Time
+[K_string_DT,rho_string_DT,feas_string_DT]=LMI_DT_Stability(F,Gd,Hd,N,ContStruc_Distr_string); % LMI for stability
+[K_string_DT_perf,rho_string_DT_perf,feas_string_DT_perf]=LMI_DT_Performance(F,Gd,Hd,N,ContStruc_Distr_string,rho_DT); % LMI for performance
+[K_string_DT_circle,rho_string_DT_circle,feas_string_DT_circle]=LMI_DT_Circle_Area(F,Gd,Hd,N,ContStruc_Centr,center,radius); % LMI for performance
+[K_string_DT_effort,rho_string_DT_effort,feas_string_DT_effort]=LMI_DT_Effort(F,Gd,Hd,N,ContStruc_Centr,alpha_L,alpha_Y);
+[K_string_DT_H2,rho_string_DT_H2,feas_string_DT_H2]=LMI_DT_H2(F,Gd,Hd,N,ContStruc_Centr);
+
+
+%% Display
+ disp('Results (Discrete-time):')
+ disp(['-  Distributed_String_DT: Feasibility=',num2str(feas_string_DT),', rho=',num2str(rho_string_DT),', FM=',num2str(string_fm_DT),'.'])
+ disp(['-  Distributed_String_DT_Perf: Feasibility=',num2str(feas_string_DT_perf),', rho=',num2str(rho_string_DT_perf),', FM=',num2str(string_fm_DT),'.'])
+ disp(['-  Distributed_String_DT_Circle: Feasibility=',num2str(feas_string_DT_circle),', rho=',num2str(rho_string_DT_circle),', FM=',num2str(string_fm_DT),'.'])
+ disp(['-  Distributed_String_DT_Effort: Feasibility=',num2str(feas_string_DT_effort),', rho=',num2str(rho_string_DT_effort),', FM=',num2str(string_fm_DT),'.'])
+ disp(['-  Distributed_String_DT_H2: Feasibility=',num2str(feas_string_DT_H2),', rho=',num2str(rho_string_DT_H2),', FM=',num2str(string_fm_DT),'.'])
+
+%% Plots
+Gtot=[];
+Htot=[];
+Btot=[];
+Ctot=[];
+for i=1:N
+    Btot=[B,Bd{i}];
+    Ctot=[C
+        Cd{i}];
+    Gtot=[G,Gd{i}];
+    Htot=[H
+        Hd{i}];
+end
+
+% simulation data
+Tfinal=10;
+T=0:0.01:Tfinal;
+% Random initial condition 
+min_x0 = 0;
+max_x0 = 1;
+random_number = min_x0 + (max_x0-min_x0) .* rand(1,1);  % Generate a random number between [min_x0, max_x0]
+x0 = repmat(random_number,36,1);
+
+% DT Simulation
+
+k = 0;
+for k=1:Tfinal/Ts
+     x_string_DT(:,k)=((F+G*K_c_DT)^k)*x0;
+     x_string_DT_perf(:,k)=((F+G*K_c_DT_perf)^k)*x0;
+     x_string_DT_circle(:,k)=((F+G*K_string_DT_circle)^k)*x0;
+     x_string_DT_effort(:,k)=((F+G*K_string_DT_effort)^k)*x0;
+     x_string_DT_H2(:,k)=((F+G*K_string_DT_H2)^k)*x0;
+end
+
+eig_DT = eig(F+G*K_string_DT_circle)
+
+% Creazione della figura
+figure;
+hold on;
+grid on;
+axis equal;
+xlim([-1.2 1.2]);
+ylim([-1.2 1.2]);
+
+% Disegna il cerchio unitario
+theta = linspace(0, 2*pi, 300);
+plot(cos(theta), sin(theta), 'k--', 'LineWidth', 1.5); % Cerchio unitario
+
+% Disegna il cerchio desiderato
+plot(-center + radius * cos(theta), radius * sin(theta), 'r-', 'LineWidth', 1.5); 
+
+% Disegna gli autovalori
+plot(real(eig_DT), imag(eig_DT), 'bx', 'MarkerSize', 10, 'LineWidth', 2);
+
+% Disegna gli assi
+plot([-1.2, 1.2], [0, 0], 'k', 'LineWidth', 1);
+plot([0, 0], [-1.2, 1.2], 'k', 'LineWidth', 1);
+
+% Titolo e legenda
+title('Autovalori e Regione di Collocazione');
+legend({'Cerchio unitario', 'Regione desiderata', 'Autovalori'}, 'Location', 'Best');
+xlabel('Re');
+ylabel('Im');
+
+hold off;
+
+
+% Plotting px1 coordinate for every LMI used
+figure
+plot([Ts:Ts:Tfinal],x_string_DT(1,:),[Ts:Ts:Tfinal],x_string_DT_perf(1,:),[Ts:Ts:Tfinal],x_string_DT_circle(1,:),[Ts:Ts:Tfinal],x_string_DT_effort(1,:),[Ts:Ts:Tfinal],x_string_DT_H2(1,:))
+title('DT controllers graphs')
+grid on
+legend('DT Stability', 'DT Performance', 'DT Circle Area', 'DT Effort', 'DT H2')
+xlabel('Time (k)')
+ylabel('Position (X)')
