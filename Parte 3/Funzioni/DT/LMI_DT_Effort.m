c80dcb5ddc4876eb1db5d026111ab0a5f96a27b2
Airoldsman <edoardo.airoldi@mail.polimi.it>
2025-02-06 14:03:12 +0100

    [LMI] : Effort and H2 copied formulation from Marco Grossoni, both to be checked

--- Parte 3/Funzioni/DT/LMI_DT_Effort.m
+++ Parte 3/Funzioni/DT/LMI_DT_Effort.m
@@ -1,12 +1,12 @@
-function [K,rho,feas]=LMI_DT_Effort(F,G,H,N,ContStruc,center,radius)
-% Computes, using LMIs, the distributed "state feedback" control law for the continuous-time system, with reference to the control
+function [K,rho,feas,k_L,k_Y]=LMI_DT_effort(F,G,H,N,ContStruc)
+% Computes, using LMIs, the distributed "state feedback" control law for the discrete-time system, with reference to the control
 % information structure specified by 'ContStruc'.
 %
 % Inputs:
-% - A: system matrix.
-% - B: input matrices (i.e., B{1},..., B{N} are the input matrices of the decomposed system, one for each channel).
-% - C: output matrices  (i.e., C{1},..., C{N} are the output matrices of the decomposed system, one for each channel, where [Cdec{1}',...,
-% Cdec{N}']=I).
+% - F: system matrix.
+% - G: input matrices (i.e., G{1},..., G{N} are the input matrices of the decomposed system, one for each channel).
+% - H: output matrices  (i.e., H{1},..., H{N} are the output matrices of the decomposed system, one for each channel, where [Hdec{1}',...,
+% Hdec{N}']=I).
 % - N: number of subsystems.
 % - ContStruc: NxN matrix that specifies the information structure
 % constraints (ContStruc(i,j)=1 if communication is allowed between channel
@@ -14,20 +14,33 @@ function [K,rho,feas]=LMI_DT_Effort(F,G,H,N,ContStruc,center,radius)
 %
 % Output:
 % - K: structured control gain
-% - rho: spectral abscissa of matrix (A+B*K) - note that [C{1}',...,
-% C{N}']=I
+% - rho: spectral radius of matrix (F+G*K) - note that [H{1}',...,
+% H{N}']=I
 % - feas: feasibility of the LMI problem (=0 if yes)
+
+for i=1:N
+    Gdec{i}=G(:,i);
+    Hdec{i}=(H(2*(i-1)+1:2*i,:))';
+end
 
 Gtot=[];
 for i=1:N
-    m(i)=size(G{i},2);
-    n(i)=size(H{i},1);
-    Gtot=[Gtot,G{i}];
+    m(i)=size(Gdec{i},2);
+    n(i)=size(Hdec{i},2);
+    Gtot=[Gtot,Gdec{i}];
 end
 ntot=size(F,1);
 mtot=sum(m);
+alpha_L = 0.01;
+alpha_Y = 10;
 
 yalmip clear
+
+k_L =sdpvar;
+k_L_sqrt = sqrt(k_L);
+k_Y = sdpvar;
+
+
 
 if ContStruc==ones(N,N)
     % Centralized design
@@ -51,12 +64,29 @@ else
     end
 end
 
-LMIconstr=[[(radius^2-center^2)*P-F*P*F'-Gtot*L*F'-F*L'*Gtot'-center*(P*F'+L'*Gtot'+Gtot*L+F*P)     Gtot*L;
-                            L'*Gtot'                                         P     ]>=-1e-2*eye(2*ntot)];
-options=sdpsettings('solver','sedumi');
-J=optimize(LMIconstr,[],options);
-feas=J.problem;
+%Control effort constrains
 
+ 
+    LMIconstr=[[P-F*P*F'-F*L'*Gtot'-Gtot*L*F' Gtot*L;
+        L'*Gtot' P]>=1e-1*eye(ntot*2)];
+
+    constr_1 = [[k_L_sqrt*eye(ntot) L';L eye(mtot)]>=1e-1*eye(ntot+mtot)];
+
+    constr_2 = [[k_Y*eye(ntot) eye(ntot);eye(ntot) P]>=1e-2*eye(ntot*2)];
+
+%Optimization
+
+constraints = [LMIconstr,constr_1,constr_2];
+objective_function = alpha_L*k_L+alpha_Y*k_Y;
+options=sdpsettings('solver','sdpt3');
+
+% Display constraints
+disp('Constraints:');
+disp(sdisplay(constraints));
+
+J=optimize(constraints,objective_function,options);
+
+feas=J.problem;
 L=double(L);
 P=double(P);
 
--- Parte 3/Funzioni/DT/LMI_DT_H2.m
+++ Parte 3/Funzioni/DT/LMI_DT_H2.m
@@ -1,4 +1,4 @@
-function [K,rho,feas]=LMI_DT_H2(F,G,H,N,ContStruc,center,radius)
+function [K,rho,feas]=LMI_DT_H2(F,G,H,N,ContStruc)
 % Computes, using LMIs, the distributed "state feedback" control law for the continuous-time system, with reference to the control
 % information structure specified by 'ContStruc'.
 %
@@ -17,17 +17,33 @@ function [K,rho,feas]=LMI_DT_H2(F,G,H,N,ContStruc,center,radius)
 % - rho: spectral abscissa of matrix (A+B*K) - note that [C{1}',...,
 % C{N}']=I
 % - feas: feasibility of the LMI problem (=0 if yes)
+Btot=[];
+
+for i=1:N
+    Gdec{i}=G(:,i);
+    Hdec{i}=(H(2*(i-1)+1:2*i,:))';
+end
 
 Gtot=[];
 for i=1:N
-    m(i)=size(G{i},2);
-    n(i)=size(H{i},1);
-    Gtot=[Gtot,G{i}];
+    m(i)=size(Gdec{i},2);
+    n(i)=size(Hdec{i},2);
+    Gtot=[Gtot,Gdec{i}];
 end
 ntot=size(F,1);
 mtot=sum(m);
 
+R_sqrt= [1 0 0;
+        0 1 0;
+        0 0 1];
+
+H_H2 = [eye(6);zeros(3,6)];
+D_H2 = [zeros(6,3);R_sqrt];
+
+
 yalmip clear
+
+S = sdpvar(mtot+ntot,mtot+ntot);
 
 if ContStruc==ones(N,N)
     % Centralized design
@@ -51,15 +67,19 @@ else
     end
 end
 
-LMIconstr=[[(radius^2-center^2)*P-F*P*F'-Gtot*L*F'-F*L'*Gtot'-center*(P*F'+L'*Gtot'+Gtot*L+F*P)     Gtot*L;
-                            L'*Gtot'                                         P     ]>=-1e-2*eye(2*ntot)];
-options=sdpsettings('solver','sedumi');
-J=optimize(LMIconstr,[],options);
-feas=J.problem;
+LMIconstr=[[P-F*P*F'-F*L'*Gtot'-Gtot*L*F' Gtot*L;
+        L'*Gtot' P]>=1e-1*eye(ntot*2)];
+H2_constr = [S H_H2*P + D_H2*L; L'*D_H2' + P*H_H2' P] >= 1e-2*(eye(15));
 
+objective_function = trace(S);
+
+constrains = [LMIconstr,H2_constr];
+options=sdpsettings('solver','sdpt3');
+J=optimize(constrains,objective_function,options);
+feas=J.problem;
 L=double(L);
 P=double(P);
 
 K=L/P;
-rho=max(abs(eig(F+Gtot*K)));
+rho=max(real(eig(F+Gtot*K)));
 
